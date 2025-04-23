7e61933c935edf4270eca9cd4b6f9364512d31df
Andy Geppert <ageppert@gmail.com>
2025-04-22 22:14:49 -0500

    Add MIDI Test project.

--- Firmware-PlatformIO/Core64-MIDI-DIY-only-test/src/main.cpp
+++ Firmware-PlatformIO/Core64-MIDI-DIY-only-test/src/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>  // Required in order to build in PlatformIO
-
 #include <PicoSoftwareSerial.h>
-
-#include "SubSystems/Serial_Port.h"
 
 int txPin = 1;  // SAO GPIO 1 (top)
 int rxPin = 2;  // SAO GPIO 2 (bottom)
 SoftwareSerial mySerial = SoftwareSerial(txPin, rxPin);
 
-// plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that
-// data values are less than 127:
 void noteOn(int cmd, int pitch, int velocity) {
   mySerial.write(cmd);
   mySerial.write(pitch);
@@ -17,19 +12,16 @@ void noteOn(int cmd, int pitch, int velocity) {
 }
 
 void setup() {
-  SerialPortSetup();
   // Set MIDI baud rate:
   mySerial.begin(31250);
-
 }
 
 void loop() {
-  Serial.print("Hello world on the hardware serial port via USB cable."); 
   // play notes from F#-0 (0x1E) to F#-5 (0x5A):
   for (int note = 28; note < 33; note++) {
     //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
     noteOn(0x90, note, 0x01);
-    delay(100);
+    delay(1500);
     //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
     noteOn(0x90, note, 0x00);
     delay(100);
